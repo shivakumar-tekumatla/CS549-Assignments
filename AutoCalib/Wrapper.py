db8235c8d6fc9739125ed7b2e8f0a5b0b1ef0596
Reanu-Keeves <55010991+Reanu-Keeves@users.noreply.github.com>
2022-10-03 19:45:51 -0700

    Add HW1 files

---
+++ AutoCalib/Wrapper.py
@@ -0,0 +1,16 @@
+"""Estimating parameters of the camera like the focal length, 
+distortion coefficients and principle point is called Camera Calibration. 
+It is one of the most time consuming and important part of any computer vision 
+research involving 3D geometry."""
+import cv2 
+import numpy as np 
+
+class AutoCalib:
+    def __init__(self,) -> None:
+        pass
+
+def main():
+    return 
+
+if __name__ == "__main__":
+    main()
